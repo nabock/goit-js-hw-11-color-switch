af8938097d52ce1a80201f6dbf64e5b84a10756f
Nabok Serhii <s.nabock@gmail.com>
2021-06-28 22:52:33 +0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,47 +1,47 @@
-// импорт  массива цветов------------------------------------
+
 import colors from "./colors.js"
 
-// массив констант------------------------------------------
+
 const refs = {
-    body: document.body,
-    btnStart: document.querySelector('[data-action="start"]'),
-    btnStop: document.querySelector('[data-action="stop"]'),
+  body: document.body,
+  btnStart: document.querySelector('[data-action="start"]'),
+  btnStop: document.querySelector('[data-action="stop"]'),
 }
-// события на кнопки---------------------------------------
+
+
 refs.btnStart.addEventListener('click', colorSwitchStart);
 
 refs.btnStop.addEventListener('click', colorSwitchStop);
 
- refs.btnStop.disabled = true;
+refs.btnStop.disabled = true;
 
 let intervalID
-// при нажатии кнопки старт---------------------------
+// start button is pressed
 function colorSwitchStart() {
-    refs.btnStart.disabled = true;
+  refs.btnStart.disabled = true;
   refs.btnStop.disabled = false;
   // console.log('START')
   return intervalID = setInterval(colorSwitch, 1000)
-  
+
 }
 
-// при нажатии кнопки стоп---------------------------
+// stop button is pressed
 
 function colorSwitchStop() {
-    refs.btnStart.disabled = false;
-    refs.btnStop.disabled = true;
-    clearInterval(intervalID);
-    // console.log('STOP')
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(intervalID);
+  // console.log('STOP')
 }
-// генерация случайного цвета---------------------------
+// color generation
 
 function colorSwitch() {
 
   const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
   };
-  let i = randomIntegerFromInterval(0, colors.length-1);
-  
+  let i = randomIntegerFromInterval(0, colors.length - 1);
+
   refs.body.style.backgroundColor = `${colors[i]}`
-  // let N = i + 1
-  //   console.log(`color ${colors[i]} by № ${N} from colors list`)
+
 }
